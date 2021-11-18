b1f26fb005c99fcd5c64df451b63054a8fadf3d2
hamzamullan <80555783+hamzamullan@users.noreply.github.com>
2021-11-17 17:52:30 -0700

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -11,7 +11,7 @@ var firebaseConfig = {
    firebase.initializeApp (firebaseConfig);
   
   user_name = localStorage.getItem("user_name");
-  document.getElementById("user_name").innerHTML = "Welcome" + user_name + "!";
+  document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
 function addRoom(){
       room_name = document.getElementById("room_name").value;
